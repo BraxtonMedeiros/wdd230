ff4539668eef2a12e830986622199dde36aeae4e
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2023-04-25 16:58:28 -0600

    Finished the team activity

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+let newDate = new Date()
+document.getElementById("copyright-year").innerText = newDate.getFullYear();
+
+let lastModified = document.lastModified
+document.getElementById("lastModified").innerText = lastModified;
